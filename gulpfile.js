529a1a8951c1aad4b758fe1a6eac902b1b4d33f0
lenoox <62141973+lenoox@users.noreply.github.com>
2022-01-02 23:25:35 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const minify = require('gulp-minify');
+
+gulp.task('sass', function() {
+    return gulp.src("assets-dev/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("assets/css"))
+        .pipe(browserSync.stream());
+});
+gulp.task('min-js', function() {
+    return gulp.src('assets-dev/js/*.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+            ignoreFiles: ['-min.js']
+        }))
+        .pipe(gulp.dest('assets/js'))
+});
+gulp.task('serve', gulp.series('sass','min-js', function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("assets-dev/scss/*.scss", gulp.series('sass'));
+    gulp.watch("assets-dev/js/*.js", gulp.series('min-js')); 
+    gulp.watch("index.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
+
+
