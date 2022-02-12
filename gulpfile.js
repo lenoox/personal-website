3d1d985aa07d939cd681d23a33f006fba124b277
lenoox <62141973+lenoox@users.noreply.github.com>
2022-02-12 16:26:20 +0100

    Changed the file folder for images, styles and code

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,36 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
-    return gulp.src("assets-dev/scss/**/*.scss")
+    return gulp.src("assets/scss/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("assets/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 gulp.task('min-js', function () {
-    return gulp.src('assets-dev/js/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(concat('script.js'))
-        .pipe(gulp.dest('assets/js'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(rename('script.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('assets/js'));
+        .pipe(gulp.dest('dist/js'));
 });
 gulp.task('build-images', function () {
-    return gulp.src(['assets-dev/images/*.{gif,jpg,png,svg}'])
+    return gulp.src(['assets/images/*.{gif,jpg,png,svg}'])
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.mozjpeg({quality: 65, progressive: true}),
             imagemin.optipng({optimizationLevel: 5}),
             imagemin.svgo({plugins: [{removeViewBox: false}]})
         ]))
-        .pipe(gulp.dest('assets/images'));
+        .pipe(gulp.dest('dist/images'));
 });
 gulp.task('serve', gulp.series('sass', 'min-js', 'build-images', function () {
     browserSync.init({
         server: "./"
     });
-    gulp.watch('assets-dev/images/*.{gif,jpg,png,svg}', gulp.series('build-images'));
-    gulp.watch("assets-dev/scss/**/*.scss", gulp.series('sass'));
-    gulp.watch("assets-dev/js/**/*.js", gulp.series('min-js'));
+    gulp.watch('assets/images/*.{gif,jpg,png,svg}', gulp.series('build-images'));
+    gulp.watch("assets/scss/**/*.scss", gulp.series('sass'));
+    gulp.watch("assets/js/**/*.js", gulp.series('min-js'));
     gulp.watch("index.html").on('change', browserSync.reload);
 }));
 
